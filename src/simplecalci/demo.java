afd45b31c24afcfa13ef931484930a570d1a5681
ram <ramtunguturi36@gmail.com>
2024-11-13 15:37:48 +0530

    Initial commit

---
+++ src/simplecalci/demo.java
@@ -0,0 +1,51 @@
+package simplecalci;
+
+import java.util.Scanner;
+
+public class demo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        char operator;
+
+        System.out.println("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.println("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("The result is: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("The result is: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("The result is: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("The result is: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero!");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        scanner.close();
+    }
+}
